de22f23f29a168e3086160acf7acd5bd0f7fd4c3
Jordan Hudgens <jordan@devcamp.com>
2022-05-04 17:58:38 -0700

    Workshop Demo

--- src/components/DeviceChart.tsx
+++ src/components/DeviceChart.tsx
@@ -4,6 +4,59 @@ import { num } from "../utils/generators";
 
 const data = new Array(3).fill(0).map((_el) => num());
 
-export default function DeviceChart() {
-  return <h2>TODO pie chart</h2>;
+type DeviceChartProps = {
+  options: {
+    labels: string[];
+    colors: string[];
+  };
+};
+export default function DeviceChart({
+  options: localOptions,
+}: DeviceChartProps) {
+  const options: ApexCharts.ApexOptions = {
+    legend: {
+      show: true,
+      position: "bottom",
+    },
+    colors: localOptions.colors,
+    stroke: { width: 0 },
+    labels: localOptions.labels,
+    dataLabels: {
+      enabled: true,
+      formatter: (val: any) => `${parseInt(val, 10)}%`,
+    },
+    plotOptions: {
+      pie: {
+        donut: {
+          labels: {
+            show: true,
+            value: {
+              formatter(val: any) {
+                return `${val}`;
+              },
+            },
+            total: {
+              show: true,
+              fontSize: "1.5rem",
+              label: "Total Visits",
+              formatter() {
+                return `${data.reduce((a, b) => a + b, 0)}`;
+              },
+            },
+          },
+        },
+      },
+    },
+  };
+
+  return (
+    <div>
+      <ReactApexcharts
+        options={options}
+        series={data}
+        type="donut"
+        height={500}
+      />
+    </div>
+  );
 }
--- src/components/Students.tsx
+++ src/components/Students.tsx
@@ -13,33 +13,83 @@ const data = {
 export default function Students() {
   const [view, setView] = React.useState<"area" | "bar">("bar");
 
+  const series = [
+    {
+      name: "James",
+      data: data.james,
+    },
+    {
+      name: "Gabrielle",
+      data: data.gabrielle,
+    },
+    {
+      name: "Alica",
+      data: data.alice,
+    },
+  ];
+
+  const options: ApexCharts.ApexOptions = {
+    xaxis: {
+      categories: [
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+      ],
+    },
+  };
+
+  const links = (
+    <div style={{ display: "flex", justifyContent: "flex-end" }}>
+      <a
+        onClick={() => setView("bar")}
+        style={{
+          fontSize: "3em",
+          color: view === "bar" ? "#0f6375" : "black",
+        }}
+      >
+        <AiOutlineBarChart />
+      </a>
+
+      <a
+        onClick={() => setView("area")}
+        style={{
+          fontSize: "3em",
+          color: view === "area" ? "#0f6375" : "black",
+        }}
+      >
+        <AiOutlineAreaChart />
+      </a>
+    </div>
+  );
+
   return (
     <Layout>
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <a
-          onClick={() => setView("bar")}
-          style={{
-            fontSize: "3em",
-            color: view === "bar" ? "#0f6375" : "black",
-          }}
-        >
-          <AiOutlineBarChart />
-        </a>
+      {links}
 
-        <a
-          onClick={() => setView("area")}
-          style={{
-            fontSize: "3em",
-            color: view === "area" ? "#0f6375" : "black",
-          }}
-        >
-          <AiOutlineAreaChart />
-        </a>
-      </div>
+      {view === "bar" && (
+        <ReactApexcharts
+          series={series}
+          options={options}
+          type="bar"
+          height={500}
+        />
+      )}
 
-      {view === "bar" && <h2>TODO: bar chart</h2>}
-
-      {view === "area" && <h2>TODO: area chart</h2>}
+      {view === "area" && (
+        <ReactApexcharts
+          series={series}
+          options={options}
+          type="area"
+          height={500}
+        />
+      )}
     </Layout>
   );
 }
